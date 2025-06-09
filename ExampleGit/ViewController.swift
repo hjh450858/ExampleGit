4bd26316e6b6975f60272dbed965982a2e82ff10
hwang jae hyeon <hjh450858@gmail.com>
2025-06-09 14:39:06 +0900

    first commit

---
+++ ExampleGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleGit
+//
+//  Created by 황재현 on 6/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExampleGitTests/ExampleGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleGitTests.swift
+//  ExampleGitTests
+//
+//  Created by 황재현 on 6/9/25.
+//
+
+import Testing
+@testable import ExampleGit
+
+struct ExampleGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
